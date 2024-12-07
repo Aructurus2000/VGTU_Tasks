2278a842146d454e36a26a26ec71494ec282f35c
arthur_burzhumov <arturb2000@mail.ru>
2024-12-07 21:04:05 +0300

    Задание 14.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { LoginPage } from './pages';
 import { RegistrationPage } from './pages/registration/RegistrationPage';
+import { AdministrationPage } from "./pages/administration/AdministrationPage";
 import { RoutesPaths } from './constants/commonConstants';
 import  { DepartmentsPage } from './pages/department';
 import './styles/globalStyles.scss'
+import { NoPermissionsPage } from './pages/noPermissions/NoPermissionsPage';
 
 export const App: React.FC = () => {
   return (
@@ -12,6 +14,8 @@ export const App: React.FC = () => {
         <Route path={RoutesPaths.Login} element={<LoginPage />} />
         <Route path={RoutesPaths.Registration} element={<RegistrationPage />} />
         <Route path={RoutesPaths.Departments} element={<DepartmentsPage />} />
+        <Route path={RoutesPaths.Administration} element={<AdministrationPage />} />
+        <Route path={RoutesPaths.NoPermissions} element={<NoPermissionsPage />} />
         <Route path={'*'} element={<LoginPage />} />
       </Routes>
   );
